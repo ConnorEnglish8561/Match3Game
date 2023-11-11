c521b268831f37189269a39555da08c81ec207ad
EvanNoelSchool <evan.noel@mymail.champlain.edu>
2023-11-10 19:09:23 -0500

    Added instructions and game over screen Combos now work for falling tiles Time now starts when the first match is made

--- Match 3 Game/Assets/Stats.cs
+++ Match 3 Game/Assets/Stats.cs
@@ -10,7 +10,9 @@ public class Stats : MonoBehaviour
     public int score, matches;
     public float remainingTime = 120f;
     public Text scoreText, matchesText, timeText;
-    
+    public Canvas gameOver;
+    public Canvas insructions;
+
     void Awake()
     {
         instance = this;
@@ -18,7 +20,8 @@ public class Stats : MonoBehaviour
 
     void Update()
     {
-        remainingTime -= Time.deltaTime;
+        if(remainingTime > 0 && score > 0)
+            remainingTime -= Time.deltaTime;
 
         scoreText.text = "Score: " + score.ToString();
         matchesText.text = "Matches: " + matches.ToString();
@@ -26,12 +29,23 @@ public class Stats : MonoBehaviour
 
         if (remainingTime <= 0f)
         {
-            BoardManager.instance.NewBoard();
+            gameOver.enabled = true;
+            //BoardManager.instance.NewBoard();
+        }
+
+        if (score == 0)
+        {
+            insructions.enabled = true;
+        }
+        else
+        {
+            insructions.enabled = false;
         }
     }
 
     public void Reset()
     {
+        gameOver.enabled = false;
         score = 0;
         remainingTime = 120f;
         matches = 0;
