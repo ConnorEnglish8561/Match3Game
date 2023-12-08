eaff23310f35bae3902d77f790f09a1beb8e1102
EvanNoelSchool <evan.noel@mymail.champlain.edu>
2023-12-08 15:32:57 -0500

    Added background to main menu and mute button to settings

--- Match 3 Game/Assets/Settings.cs
+++ Match 3 Game/Assets/Settings.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
+    bool sounds = true;
+    public TextMeshProUGUI soundButton;
+
+    public void Start()
+    {
+        if (AudioListener.pause == true)
+        {
+            sounds = false;
+            soundButton.text = "Muted";
+        }
+    }
     public void toggleFullscreen()
     {
         Screen.fullScreen = !Screen.fullScreen;
@@ -12,5 +25,24 @@ public class Settings : MonoBehaviour
     public void quitGame()
     {
         Application.Quit();
+    }
+
+    public void toggleVolume()
+    {
+        sounds = !sounds;
+
+        if (sounds)
+        {
+            AudioListener.pause = false;
+            AudioListener.volume = 1;
+            soundButton.text = "Toggle Sounds";
+        }
+
+        else
+        {
+            AudioListener.pause = true;
+            AudioListener.volume = 0;
+            soundButton.text = "Muted";
+        }
     }
 }
